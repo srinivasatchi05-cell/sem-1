2def5b7c144e7d9dbc912eac3027a97e35bf5488
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:30:08 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,111 @@
+//	 write c program to print number is palindrome or not?
+#include<stdio.h>
+int main()
+{
+	int r,c,x=0,n;
+	printf("enter a number");
+	scanf("%d",&n);
+	c=n;
+	while(n>0)
+	{
+		r=n%10;
+		x=x*10+r;
+		n=n/10;
+	}
+	if(c==x)
+	{
+		printf("the number is palindrome");
+	}
+	else
+	{
+		printf("the number is not palindrome");
+		
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
