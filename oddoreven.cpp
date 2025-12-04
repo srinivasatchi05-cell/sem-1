0af36b1eb730bd22730af90189887076bc020687
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:29:17 +0530

    Add files via upload

---
+++ oddoreven.cpp
@@ -0,0 +1,17 @@
+// write a cprogram to find the number is odd(or)even? 
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+
+    return 0;
+}
+
