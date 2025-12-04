cb3dbeb0b7a2fca1d452d0889a62bb4f5ec4214b
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:24:48 +0530

    Add files via upload

---
+++ float.c
@@ -0,0 +1,11 @@
+/*write a program initialize value of an integer and float and add both */
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int a=76;
+	float b=56.87;
+	float c;
+	c=a+b;
+	printf("%f",c);
+	return 0;
+}
