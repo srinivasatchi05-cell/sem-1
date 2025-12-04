146b22e8bce2fa9755e95835d6a1e240507cbc77
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:16:44 +0530

    Add files via upload

---
+++ byte size of c,i,d.c
@@ -0,0 +1,10 @@
+// write c program to print size of data types?
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	printf("size char:%zu byte", sizeof(long));
+	printf("\n size of int:%zu bytes",sizeof(int));
+	printf("\n size of float:%zu bytes",sizeof(float));
+	printf("\n size of double:%zu bytes",sizeof(double));
+	return 0;
+}
