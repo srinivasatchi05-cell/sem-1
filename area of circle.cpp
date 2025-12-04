479096cc72f92c60978cc1cf82bb9b3f6b6995f2
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:13:31 +0530

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,18 @@
+// write a  cprogram to find area of circle?
+#include <stdio.h>
+
+#define PI 3.14
+
+int main() {
+    float radius, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+
+    printf("Area of the circle: %.2f\n", area);
+
+    return 0;
+}
+
