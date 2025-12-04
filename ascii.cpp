993853bc2fdc94e4597f2297efe9f3b031fd1638
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:14:55 +0530

    Add files via upload

---
+++ ascii.cpp
@@ -0,0 +1,9 @@
+//write c program to print ascii value of all characters ?
+#include<stdio.h>
+int main()
+{
+	char c;
+	printf(" Enter a charrcter: ");
+	scanf("%c",&c);
+	printf("the ascii for %c is %d",c,c);
+}
