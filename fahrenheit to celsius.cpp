e863ab72bb3c4dad766173947f8a12f9994d13b5
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:23:59 +0530

    Add files via upload

---
+++ fahrenheit to celsius.cpp
@@ -0,0 +1,16 @@
+//write a cprogram to convert fahrenheit to celsius?
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    printf("Temperature in Celsius: %.2f\n", celsius);
+
+    return 0;
+}
+
