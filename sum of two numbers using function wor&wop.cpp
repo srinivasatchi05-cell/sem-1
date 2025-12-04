d2a1a599a2a2b575f496c6ad3f13fa15a4a0099e
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:38:23 +0530

    Add files via upload

---
+++ sum of two numbers using function wor&wop.cpp
@@ -0,0 +1,9 @@
+// write c program to print sum of two numbers using function with out return type & with out parameters? 
+#include <stdio.h>
+void add(){
+	int a=7,b=9;
+		printf("sum of%d+%d=%d",a,b,a+b);
+}
+int main(){
+	add();	
+}
