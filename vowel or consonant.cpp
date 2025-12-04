e303706d630b59064eb7f632394352482353c71e
atchii <srinivasatchi05@gmail.com>
2025-12-04 10:42:40 +0530

    Add files via upload

---
+++ vowel or consonant.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdio.h>
+// write c program to print letter is vowel or consonant ?
+int main() {
+    char ch;
+
+    printf("Enter an alphabet: ");
+    scanf("%c", &ch);
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            printf("%c is a vowel.\n", ch);
+        } else {
+            printf("%c is a consonant.\n", ch);
+        }
+
+    } else {
+        printf("%c is not an alphabet.\n", ch);
+    }
+
+    return 0;
+}
+
